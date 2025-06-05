86d4d9ac9a819fd0b30c57caed4690b876c71566
gabriela-c1 <gabriela.c1@aluno.ifsc.edu.br>
2025-06-05 18:09:47 +0000

    correção no código

--- cliente/js/preload.js
+++ cliente/js/preload.js
@@ -4,7 +4,7 @@ export default class abertura extends Phaser.Scene {
     super('preload')
   }
 
-  init() {
+  init () {
     this.add.rectangle(400, 300, 468, 32).setStrokeStyle(1, 0xffffff)
     const progresso = this.add.rectangle(400 - 230, 300, 4, 28, 0xffffff)
     this.load.on('progress', (progress) => {
@@ -12,7 +12,7 @@ export default class abertura extends Phaser.Scene {
     })
   }
 
-  preload() {
+  preload () {
     this.load.spritesheet('botao', 'assets/botao.png', {
       frameWidth: 64,
       frameHeight: 64
@@ -35,41 +35,42 @@ export default class abertura extends Phaser.Scene {
     })
     this.load.spritesheet('fantasmas', 'assets/fantasmas.png', {
       frameWidth: 50,
-      frameHeight: 64)
+      frameHeight: 64
     })
     this.load.spritesheet('tiro', 'assets/UI/shootUI.png', {
-      frameWidth: 64,
-      frameHeight: 64
+    frameWidth: 64,
+    frameHeight: 64
     })
     this.load.spritesheet('corrida', 'assets/UI/runUI.png', {
       frameWidth: 64,
       frameHeight: 64
     })
-        this.load.spritesheet('tela-cheia', 'assets/UI/tela-cheia.png', {
+    this.load.spritesheet('tela-cheia', 'assets/UI/tela-cheia.png', {
       frameWidth: 32,
       frameHeight: 32
     })
-    this.load.image('ponteiro', 'assets/UI/seta.png')
 
-    
-    this.load.image('fundo', 'assets/abertura-fundo.png')
-    this.load.image('chao', 'assets/mapa/texturas/chao/chao.png')
-    this.load.image('arvores-verdes', 'assets/mapa/texturas/objetos/arvores-verdes.png')
-    this.load.image('tendaLLD', 'assets/mapa/texturas/objetos/tendaLLD.png')
-    this.load.image('tenda', 'assets/mapa/texturas/objetos/tenda.png')
+this.load.image('ponteiro', 'assets/UI/seta.png')
 
-    this.load.image('lanterna', 'assets/luz.png')
-    this.load.image('particula-chuva', 'assets/mapa/texturas/chuva.png')
 
-    this.load.audio('chuva', 'assets/audio/chuva.wav')
-    this.load.audio('trilha-sonora', 'assets/audio/trilha-sonora.mp3')
-    this.load.audio('passos', 'assets/audio/passos.mp3')
-    this.load.audio('tiro', 'assets/audio/tiro.mp3')
+this.load.image('fundo', 'assets/abertura-fundo.png')
+this.load.image('chao', 'assets/mapa/texturas/chao/chao.png')
+this.load.image('arvores-verdes', 'assets/mapa/texturas/objetos/arvores-verdes.png')
+this.load.image('tendaLLD', 'assets/mapa/texturas/objetos/tendaLLD.png')
+this.load.image('tenda', 'assets/mapa/texturas/objetos/tenda.png')
+
+this.load.image('lanterna', 'assets/luz.png')
+this.load.image('particula-chuva', 'assets/mapa/texturas/chuva.png')
+
+this.load.audio('chuva', 'assets/audio/chuva.wav')
+this.load.audio('trilha-sonora', 'assets/audio/trilha-sonora.mp3')
+this.load.audio('passos', 'assets/audio/passos.mp3')
+this.load.audio('tiro', 'assets/audio/tiro.mp3')
   }
 
-  create() {
-    this.scene.start('sala')
-  }
+create() {
+  this.scene.start('sala')
+}
 
-  update() { }
+update() { }
 }
